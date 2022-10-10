28a3ffc77c2cc7d7e78e4dc4418a26e1c2f580d6
Fatima <Fatima@Moran>
2022-10-10 17:30:12 +0200

    [MOD] acumulador de donaciones por organizacion

--- PracticaDonaciones/js/script.js
+++ PracticaDonaciones/js/script.js
@@ -1,7 +1,7 @@
-var arrayOrganizacioness=["Save The Children","Caritas","Cruz Roja","Caritas", "Caritas", "Caritas", "Caritas", "Caritas","Caritas","Caritas"];
+var arrayOrganizacioness=["Save the children","Caritas","Cruz Roja","GreenPeace", "Medicos sin fronteras", "Caritas", "Caritas", "Caritas","Caritas","Caritas"];
 var arrayDonaciones=[10,2,3,5,2,3,5,6,3,4];
 var arrayDonacionesPorOrganizacion=[0,0,0,0,0,0,0,0,0,0];
-
+var arrayAportaciones=[0,0,0,0,0,0,0,0,0,0];
 function donacionPorOrganizacion(organizacion){
    
  let indice = Number(arrayOrganizacioness.findIndex(posicion=> posicion == organizacion));
@@ -10,6 +10,15 @@ function donacionPorOrganizacion(organizacion){
  arrayDonacionesPorOrganizacion[indice]= arrayDonacionesPorOrganizacion[indice]+arrayDonaciones[indice];
 
    console.log(arrayDonacionesPorOrganizacion);
+}
+function NumAportacionesPorOrganizacion(organizacion){
+  let indice = Number(arrayOrganizacioness.findIndex(posicion=> posicion == organizacion));
+  arrayAportaciones[indice]= arrayAportaciones[indice]+1;
+  console.log(arrayAportaciones);
+}
+
+function MediaTotalDonaciones(){
+
 }
 
 function sumaTotalDeDonaciones(){
