605492cc7759ce8e824d7d128af3139f59a7ef02
Fatima <Fatima@Moran>
2022-10-10 17:06:20 +0200

    [ADD] funcionamiento de suma total de donaciones

--- PracticaDonaciones/js/script.js
+++ PracticaDonaciones/js/script.js
@@ -1,6 +1,6 @@
-var arrayOrganizacioness=["h","v","lo"];
-var arrayDonaciones=[10,2,3,5,2,3,5,6,3,4,5];
-var arrayDonacionesPorOrganizacion=[0,0,0,0,0,0,0];
+var arrayOrganizacioness=["Save The Children","Caritas","Cruz Roja","Caritas", "Caritas", "Caritas", "Caritas", "Caritas","Caritas","Caritas"];
+var arrayDonaciones=[10,2,3,5,2,3,5,6,3,4];
+var arrayDonacionesPorOrganizacion=[0,0,0,0,0,0,0,0,0,0];
 
 function donacionPorOrganizacion(organizacion){
    
@@ -10,4 +10,12 @@ function donacionPorOrganizacion(organizacion){
  arrayDonacionesPorOrganizacion[indice]= arrayDonacionesPorOrganizacion[indice]+arrayDonaciones[indice];
 
    console.log(arrayDonacionesPorOrganizacion);
+}
+
+function sumaTotalDeDonaciones(){
+  let totalDonaciones= arrayDonacionesPorOrganizacion.reduce(function(total, item){
+    total=total+item;
+    return total;
+  })
+  console.log(totalDonaciones);
 }
