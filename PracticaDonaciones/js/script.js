d445046178c4860176fe4f334d6b3d185dbedb79
Fatima <Fatima@Moran>
2022-10-18 09:29:19 +0200

    [ADD]Añadir comentarios

--- PracticaDonaciones/js/script.js
+++ PracticaDonaciones/js/script.js
@@ -13,6 +13,9 @@ var arrayOrganizacioness = [
 var arrayDonaciones = [10, 2, 3, 5, 2, 8, 5, 6, 3, 4];
 var arrayAportaciones = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+
+/*Esta función rellena el array de aportaciones segun las veces que se le de click a una organizacion.
+Al final de la función borra el contenido de texto si hubiera algo dentro de la caja de texto*/
 function NumAportacionesPorOrganizacion(organizacion) {
   let indice = Number(
     arrayOrganizacioness.findIndex((posicion) => posicion == organizacion)
@@ -21,6 +24,8 @@ function NumAportacionesPorOrganizacion(organizacion) {
   BorrarContenidoDiv();
 }
 
+/*Crea un array que une las organizaciones con las aportaciones realizadas por cada organización 
+y luego los ordena al contrario del orden alfabetico. Si el numero de aportación es 0 no lo mete en el nuevo array*/ 
 function UnirOrganizacionYAportacion() {
   let arrayOrganizacionConAportacion = [];
   arrayOrganizacionConAportacion.length = 10;
@@ -40,6 +45,8 @@ function UnirOrganizacionYAportacion() {
   return arrayOrganizacionConAportacion;
 }
 
+/*Hace la suma total de aportaciones que se han hecho sumando las posiciones del array de 
+aportaciones para tener el numero total de aportaciones y asi poder hacer la media*/
 function MediaTotalDonaciones() {
   let numeroAportaciones = 0;
   for (var i = 0; i <= arrayAportaciones.length - 1; i++) {
@@ -51,6 +58,7 @@ function MediaTotalDonaciones() {
   return mediaDonaciones;
 }
 
+ 
 function SumaTotalDeDonaciones() {
   let totalDonaciones = 0;
   for (var i = 0; i <= arrayAportaciones.length - 1; i++) {
@@ -59,14 +67,17 @@ function SumaTotalDeDonaciones() {
   }
   return totalDonaciones;
 }
+
+
+/*Inicializa el array de aportaciones a 0 para poder volver a empezar de nuevo las donaciones */
 function InicializarTodo(){
   for(var i=0; i<=arrayAportaciones.length-1;i++){
     arrayAportaciones[i]=0;
   }
 }
 
-function CrearTexto() {
 
+function CrearTexto() {
   let caja = document.getElementById("CajaDeTexto");
   for (var i = 0; i <= arrayOrganizacioness.length - 1; i++) {
     let parrafo = document.createElement("p");
