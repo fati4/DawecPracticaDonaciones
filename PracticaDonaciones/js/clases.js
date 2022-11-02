84469cfe789c8887643c471b8fcd24bdd94e0fa5
Fatima <Fatima@Moran>
2022-11-02 17:03:43 +0100

    [ADD]clases organizacion

---
+++ PracticaDonaciones/js/clases.js
@@ -0,0 +1,40 @@
+class Organizacion{
+    constructor(nombre, cantidad){
+        this.nombre=nombre;
+        this.cantidad= cantidad;
+    }
+    getNombre(){
+        return this.nombre;
+    }
+    getCantidad(){
+        return this.cantidad;
+    }
+
+}
+
+class OAnimales extends Organizacion{
+ constructor(nombre, cantidad, multiraza, ambitoTrabajo ){
+    super(nombre, cantidad)
+    this.multiraza=multiraza;
+    this.ambitoTrabajo=ambitoTrabajo;
+ }
+
+ getambitoTrabajo(){
+    if (this.ambitoTrabajo=="local"  || this.ambitoTrabajo=="provincial" || this.ambitoTrabajo=="nacional" || this.ambitoTrabajo=="internacional"){
+       return this.ambitoTrabajo  
+    }else{
+        console.log("error")
+    }
+   
+ }
+}
+
+class OPersonas extends Organizacion{
+    constructor(nombre, cantidad, colaborar, rangoEdad ){
+        super(nombre, cantidad) 
+        this.colaborar=colaborar;
+        this.rangoEdad=rangoEdad;
+    }
+
+    
+}
