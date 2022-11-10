02977a47f342cd18d3055780e5fbf19cc806a6aa
Fatima <Fatima@Moran>
2022-11-10 13:32:03 +0100

    [MOD]marcar parrafo lateral segun organizacion

--- PracticaDonaciones/js/script.js
+++ PracticaDonaciones/js/script.js
@@ -1,4 +1,3 @@
-
 /*var arrayOrganizacioness = [
   "Save the children",
   "Caritas",
@@ -16,18 +15,89 @@
 var arrayTotalDonacion = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var arrayAportaciones = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-var SaveTheChildren = new OPersonas("Save the children", arrayAportaciones[0], 0, 0, true, "infantil");
-var Caritas = new OPersonas("Caritas", arrayAportaciones[1], 0, 0, false, "adolescencia");
-var CruzRoja = new OPersonas("Cruz Roja", arrayAportaciones[2], 0, 0, false, "infantil");
-var GreenPeace = new OAnimales("GreenPeace", arrayAportaciones[3], 0, 0, true, "nacional");
-var MedicosSinFrontera = new OPersonas("Medicos sin fronteras", arrayAportaciones[4], 0, 0, false, " tercera edad");
-var AccionContraElHambre = new OAnimales("Accion contra el hambre", arrayAportaciones[5], 0, 0, true, "provincial");
-var AmnistiaInternacional = new OPersonas("Amnistía Internacional", arrayAportaciones[6], 0, 0, true, "adolescencia");
-var AsociacionEspanolaContraElCancer = new OPersonas("Asociacion Española contra el cáncer", arrayAportaciones[7], 0, 0, false, "tercera edad");
-var WWF = new OAnimales("WWF", arrayAportaciones[8], 0, 0, true, "internacional");
-var Unicef = new OAnimales("Unicef", arrayAportaciones[9], 0, 0, false, "local");
+var SaveTheChildren = new OPersonas(
+  "Save the children",
+  arrayAportaciones[0],
+  0,
+  0,
+  true,
+  "infantil"
+);
+var Caritas = new OPersonas(
+  "Caritas",
+  arrayAportaciones[1],
+  0,
+  0,
+  false,
+  "adolescencia"
+);
+var CruzRoja = new OPersonas(
+  "Cruz Roja",
+  arrayAportaciones[2],
+  0,
+  0,
+  false,
+  "infantil"
+);
+var GreenPeace = new OAnimales(
+  "GreenPeace",
+  arrayAportaciones[3],
+  0,
+  0,
+  true,
+  "nacional"
+);
+var MedicosSinFrontera = new OPersonas(
+  "Medicos sin fronteras",
+  arrayAportaciones[4],
+  0,
+  0,
+  false,
+  " tercera edad"
+);
+var AccionContraElHambre = new OAnimales(
+  "Accion contra el hambre",
+  arrayAportaciones[5],
+  0,
+  0,
+  true,
+  "provincial"
+);
+var AmnistiaInternacional = new OPersonas(
+  "Amnistía Internacional",
+  arrayAportaciones[6],
+  0,
+  0,
+  true,
+  "adolescencia"
+);
+var AsociacionEspanolaContraElCancer = new OPersonas(
+  "Asociacion Española contra el cáncer",
+  arrayAportaciones[7],
+  0,
+  0,
+  false,
+  "tercera edad"
+);
+var WWF = new OAnimales(
+  "WWF",
+  arrayAportaciones[8],
+  0,
+  0,
+  true,
+  "internacional"
+);
+var Unicef = new OAnimales(
+  "Unicef",
+  arrayAportaciones[9],
+  0,
+  0,
+  false,
+  "local"
+);
 
-var arrayOrganizaciones = [SaveTheChildren,
+var arrayOrganizaciones = [
+  SaveTheChildren,
   Caritas,
   CruzRoja,
   GreenPeace,
@@ -36,13 +106,16 @@ var arrayOrganizaciones = [SaveTheChildren,
   AmnistiaInternacional,
   AsociacionEspanolaContraElCancer,
   WWF,
-  Unicef];
+  Unicef,
+];
 
 function totalDonacionOrganizacion(organizacion) {
   let indice = Number(
-    arrayOrganizaciones.findIndex((posicion) => posicion.getNombre() == organizacion)
+    arrayOrganizaciones.findIndex(
+      (posicion) => posicion.getNombre() == organizacion
+    )
   );
-  let aportacion = document.getElementsByName('donacion')[indice].value;
+  let aportacion = document.getElementsByName("donacion")[indice].value;
   console.log(aportacion);
   let total;
   total = Number(arrayOrganizaciones[indice].getTotal()) + Number(aportacion);
@@ -55,19 +128,23 @@ function totalDonacionOrganizacion(organizacion) {
 Al final de la función borra el contenido de texto si hubiera algo dentro de la caja de texto*/
 function NumAportacionesPorOrganizacion(organizacion) {
   let indice = Number(
-    arrayOrganizaciones.findIndex((posicion) => posicion.getNombre() == organizacion)
+    arrayOrganizaciones.findIndex(
+      (posicion) => posicion.getNombre() == organizacion
+    )
   );
-  let aportacion = document.getElementsByName('donacion')[indice].value;
+  let aportacion = document.getElementsByName("donacion")[indice].value;
   if (aportacion > 0) {
     let indice = Number(
-      arrayOrganizaciones.findIndex((posicion) => posicion.getNombre() == organizacion)
+      arrayOrganizaciones.findIndex(
+        (posicion) => posicion.getNombre() == organizacion
+      )
     );
     arrayAportaciones[indice] = arrayAportaciones[indice] + 1;
     totalDonacionOrganizacion(organizacion);
     rellenarContenedorLateral(organizacion, aportacion);
-    marcarOrganizacion();
+    marcarOrganizacion(organizacion);
     calcularMedia(organizacion);
-    
+
     console.log(arrayAportaciones);
   }
 
@@ -77,21 +154,20 @@ function NumAportacionesPorOrganizacion(organizacion) {
 function rellenarContenedorLateral(organizacion, aportacion) {
   var cajaLateral = document.getElementById("contenedorLateral");
   let parrafoDonacion = document.createElement("p");
-  parrafoDonacion.style.backgroundColor = ("pink");
+  parrafoDonacion.style.backgroundColor = "pink";
   parrafoDonacion.textContent = organizacion + " --- " + aportacion;
   cajaLateral.appendChild(parrafoDonacion);
 }
-function marcarOrganizacion() {
+function marcarOrganizacion(organizacion) {
   var cajaLateral = document.getElementById("contenedorLateral");
   let parrafosL = cajaLateral.children;
   for (var i = 0; i < parrafosL.length; i++) {
-    let pFinal = parrafosL[parrafosL.length - 1].textContent;
     let pI = parrafosL[i].textContent;
-    if (pFinal == pI) {
-      parrafosL[i].style.color = ("green");
-
+    let contiene = pI.search(organizacion);
+    if (contiene != -1) {
+      parrafosL[i].style.color = "green";
     } else {
-      parrafosL[i].style.color = ("black");
+      parrafosL[i].style.color = "black";
     }
   }
 }
@@ -103,29 +179,31 @@ function fechaFinalizacion() {
 
 function calcularMedia(organizacion) {
   let indice = Number(
-    arrayOrganizaciones.findIndex((posicion) => posicion.getNombre() == organizacion)
+    arrayOrganizaciones.findIndex(
+      (posicion) => posicion.getNombre() == organizacion
+    )
   );
-  let media = arrayOrganizaciones[indice].getTotal() / arrayAportaciones[indice];
+  let media =
+    arrayOrganizaciones[indice].getTotal() / arrayAportaciones[indice];
   media.toFixed(3);
   arrayOrganizaciones[indice].setMedia(media);
   console.log(arrayOrganizaciones[indice].getMedia());
   return media;
-
 }
 
 function InvertirOrderOrganizaciones() {
-
-  let arrayOrganizacionesOrdenadas= arrayOrganizaciones.sort(function(a, b){
-    if(a.nombre> b.nombre){
+  let arrayOrganizacionesOrdenadas = arrayOrganizaciones.sort(function (a, b) {
+    if (a.nombre > b.nombre) {
       return 1;
-    } if(a.nombre< b.nombre){
-      return -1;
-    }else{
-  return 0;
     }
-  })
+    if (a.nombre < b.nombre) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
   arrayOrganizacionesOrdenadas.reverse();
- return arrayOrganizacionesOrdenadas;
+  return arrayOrganizacionesOrdenadas;
 }
 
 /*Crea un array que une las organizaciones con las aportaciones realizadas por cada organización 
@@ -187,13 +265,18 @@ function InicializarTodo(){
 
 function CrearTexto() {
   let caja = document.getElementById("CajaDeTexto");
-  let fecha= document.createElement("p");
-  fecha.textContent="Fecha de compra: "+fechaFinalizacion();
+  let fecha = document.createElement("p");
+  fecha.textContent = "Fecha de compra: " + fechaFinalizacion();
   caja.appendChild(fecha);
 
-  for (var i = 0; i <  arrayOrganizaciones.length; i++) {
+  for (var i = 0; i < arrayOrganizaciones.length; i++) {
     let parrafo = document.createElement("p");
-    parrafo.textContent = InvertirOrderOrganizaciones()[i].getNombre() + " --- " + InvertirOrderOrganizaciones()[i].getMedia() + " --- " + InvertirOrderOrganizaciones()[i].getTotal();
+    parrafo.textContent =
+      InvertirOrderOrganizaciones()[i].getNombre() +
+      " --- " +
+      InvertirOrderOrganizaciones()[i].getMedia() +
+      " --- " +
+      InvertirOrderOrganizaciones()[i].getTotal();
     caja.appendChild(parrafo);
   }
   /*let parrafoSuma = document.createElement("p");
@@ -216,6 +299,3 @@ function BorrarContenidoDiv() {
 }
 
 */
-
-
-
